f4d6e0dc5679f407791e36efec9c4972dfb61c0b
hg2896 <hg2896@nyu.edu>
2024-06-23 13:18:45 -0700

    final i hope

--- DNSClient.py
+++ DNSClient.py
@@ -1,7 +1,7 @@
 import dns.resolver
 
 # Set the IP address of the local DNS server and a public DNS server
-local_host_ip = "216.165.61.24"
+local_host_ip = "216.165.61.24"  #NYU local DNS
 real_name_server = "172.64.46.124"  # Cloudflare NY
 
 
